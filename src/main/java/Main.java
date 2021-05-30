908a61f2b9d634383261ca4298c029b061747ce0
Grandius <gradius777@gmail.com>
2021-05-30 20:49:58 +0300

    Commit cleanup

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,17 @@ public class Main {
 
     public static void main(String[] args) {
 
+        KnightChess[] knights = new KnightChess[4];
+        knights[0] = new KnightChess();
+        knights[1] = new KnightChess("B6");
+        knights[2] = new KnightChess("H4");
+        knights[3] = new KnightChess("A2");
+        for (KnightChess kn: knights) {
+
+            kn.moveFigure("C3");
+
+        }
+
 
     }
 
